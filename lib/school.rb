6bccce1a6b31642e9fd48defd6e7b11948f51ffb
Don Mallory <dmm333@gmail.com>
2020-04-21 20:01:11 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+    attr_accessor :roster
+
+    def initialize(name)
+        @roster = Hash.new
+    end
+
+    def add_student(name, grade)
+        @roster[grade] = [] if @roster[grade].nil?
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.reduce({}) do |memo, (grade, students)|
+            memo[grade] = students.sort
+            memo
+        end
+    end
+end
